35881869990ea97973a8280fc53d7a28b168ecfe
Juan Pablo Arr <ju.arriagadar@duocuc.cl>
2024-09-26 09:57:57 -0300

    interface para la api

---
+++ src/app/interfaces/comidas.ts
@@ -0,0 +1,10 @@
+export interface Categorias {
+    categoria: Categoria[];
+}
+
+export interface Categoria {
+    idCategory: string;
+    strCategory: string;
+    strCategoryThumb: string;
+    strCategoryDescription: string;
+}
