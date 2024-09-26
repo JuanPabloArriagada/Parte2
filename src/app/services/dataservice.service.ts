26fb95a4034ccf33b5aad11271c4f8f6366c44f3
Juan Pablo Arr <ju.arriagadar@duocuc.cl>
2024-09-26 10:04:28 -0300

    para sacar la info de la api y dejarla en el array

--- src/app/services/dataservice.service.ts
+++ src/app/services/dataservice.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Categorias } from '../interfaces/comidas';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class DataserviceService {
   constructor(private httpCliente:HttpClient) { }
 
   getCategorias(){
-    return this.httpCliente.get(`https://www.themealdb.com/api/json/v1/1/categories.php`); /* las ` se ponen con alt gr + ]}` */
+    return this.httpCliente.get<Categorias>(`https://www.themealdb.com/api/json/v1/1/categories.php`); /* las ` se ponen con alt gr + ]}` */
   }
 }
